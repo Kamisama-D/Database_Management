109ea430c0f282648a59b2fd73f93bdd58cfb1df
S.Ding <46885056+Kamisama-D@users.noreply.github.com>
2024-05-23 13:40:41 -0400

    Add files via upload

---
+++ musicstoreapp/ViewModels/AnalysisViewModel.cs
@@ -0,0 +1,15 @@
+﻿using musicstoreapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace musicstoreapp.ViewModels
+{
+    public class AnalysisViewModel
+    {
+        public List<TopProduct> TopProduct { get; set; }
+        public List<SalesAndProfits> SalesAndProfits { get; set; }
+        public List<RegionSales> RegionSales { get; set; }
+    }
+}
